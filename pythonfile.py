9123b6062a3e62dc711bd40a25161c48f7068c54
sin383 <136421080+sin383@users.noreply.github.com>
2024-05-23 21:54:44 +1000

    Update pythonfile.py
    Changes made:
    - lessons started
    - testing buttons for scrollable frame

--- pythonfile.py
+++ pythonfile.py
@@ -3,6 +3,7 @@ import customtkinter as ctk
 from PIL import Image
 import PIL
 from os import path
+import os
 
 root = ctk.CTk()
 root.geometry("1000x600")
@@ -48,8 +49,8 @@ def Lessonswindows():
     
     lessonframe = ctk.CTkFrame(image_frame)
     lessonframe.place(relx=0.04, rely=0.04, relwidth=0.9, relheight=0.9)
-    lessontypeframe = ctk.CTkScrollableFrame(lessonframe, corner_radius=10, bg_color="Blue")
-    lessontypeframe.place(x=0, y=0, relwidth=0.3, relheight=1)
+    lessontypeframe = ctk.CTkScrollableFrame(lessonframe, corner_radius=10)
+    lessontypeframe.place(x=0, y=0)
     Lessoncontentframe = ctk.CTkFrame(lessonframe, corner_radius=10)
     Lessoncontentframe.place(x=280, y=0, relwidth=0.7, relheight=1)
     label_frame.place_forget()
@@ -63,16 +64,43 @@ def Lessonswindows():
     tempbutton.place(x=350, y=10)
     tempbutton2 = ctk.CTkButton(lessontypeframe, text="7")
     tempbutton2.place(x=50, y=50)   
+    def next1():
+        label_frame.place(relx=0.04, rely=0.04, relwidth=0.9, relheight=0.9)
+        lessonframe.place_forget()
+    lessonnext1button = ctk.CTkButton(lessonframe, text="Next", command=next1)
+    lessonnext1button.place(x=600, y=500)
+
+    #scrollable frame buttons
+    option1 = ctk.CTkButton(master=lessontypeframe, text="7")
+    option1.place(x=15, y=0)
+    option2 = ctk.CTkButton(master=lessontypeframe, text="7")
+    option2.place(x=15, y=50)
+    option3 = ctk.CTkButton(master=lessontypeframe, text="7")
+    option3.place(x=15, y=150)
+    option4 = ctk.CTkButton(master=lessontypeframe, text="7")
+    option4.place(x=15, y=250)
+    option5 = ctk.CTkButton(lessontypeframe, text="7")
+    option5.place(x=15, y=450)
+    option6 = ctk.CTkButton(master=lessontypeframe, text="7")
+    option6.place(x=15, y=650)
+    option7 = ctk.CTkButton(master=lessontypeframe, text="7")
+    option7.place(x=15, y=350)
+    option8 = ctk.CTkButton(master=lessontypeframe, text="7")
+    option8.place(x=5, y=50)
+
+
+
 
 
 def Settingsmenu():
+    global optionmenu_1
     label_frame.place_forget()
     Settingsframe = ctk.CTkFrame(image_frame)
     Settingsframe.place(x=250, y=30, relwidth=0.5, relheight=0.9)
 
     def backsettings():
         Settingsframe.place_forget()
-        label_frame.place(relx=0.04, rely=0.04, relwidth=0.4, relheight=0.9)
+        label_frame.place(relx=0.04, rely=0.04, relwidth=0.9, relheight=0.9)
 
     settingsbackbutton = ctk.CTkButton(Settingsframe, text="Back", command=backsettings)
     settingsbackbutton.place(x=340, y=500)
@@ -98,6 +126,25 @@ def Settingsmenu():
 
     current_theme = ctk.get_appearance_mode()
 
+    def change_scaling_event(selection):
+        scale = int(selection.strip('%')) / 100
+        ctk.set_widget_scaling(scale)
+        if selection == "125%":
+            root.geometry("1300x750")
+        elif selection == "100%":
+            root.geometry("1000x600")
+        elif selection == "150%":
+            root.geometry("1400x800")
+        elif selection == "75%":
+            root.geometry("800x500")
+        elif selection == "50%":
+            root.geometry("500x350")
+
+    optionmenu_1 = ctk.CTkOptionMenu(Settingsframe, values=["50%", "75%", "100%", "125%", "150%"], command=change_scaling_event)
+    optionmenu_1.place(x=150, y=250)
+    OptionLabel = ctk.CTkLabel(Settingsframe, text= "UI Scaling")
+    OptionLabel.place(x=150, y=220)
+    
 
 # Creating lessons and settings button
 mybutton = ctk.CTkButton(label_frame, width=450, height=64, border_width=0,corner_radius=8,text="Lessons", command=Lessonswindows)
